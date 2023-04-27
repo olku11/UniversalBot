27350afa225854295aa536ca62ead135a2d778f1
olku11 <artem.chastukhin@gmail.com>
2023-04-27 22:34:30 +0300

    kvest

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import aiohttp
 from telegram.ext import Application, MessageHandler, filters, CommandHandler, ConversationHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 
-
 # Запускаем логгирование
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
@@ -160,6 +159,119 @@ async def stop(update, context):
     return ConversationHandler.END
 
 
+async def challenge(update, context):
+    reply_keyboard = [['1', '2', '3']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    await update.message.reply_text(
+        "Добро пожаловть в квест!\n"
+        "Вам необходимо пройти небольшое текстое испытание, посредством выбор дверей\n"
+        "Только одна из них ведет на свободу, после других вас ждет смерть\n"
+        "Вы можете прервать квест, послав команду /stop.\n"
+        "Вы просыпаетесь в заброшенной теммнице. Здесь сыровато\n"
+        "На полу вы находите ключ и выбираетесь из темницы\n"
+        "Перед вами три двери. За первой дврью вы слышите детский плач.\n"
+        "За второй дверью слыше звон монет, а за третьей вы слышите лишь пустоту\n"
+        "Куда пойдем?",
+        reply_markup=markup)
+
+    return 1
+
+
+async def chal_1(update, context):
+    if update.message.text == "1":
+        reply_keyboard = [['1', '2', '3']]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        await update.message.reply_text(
+            "Детский плач оказался мелким ручейком. Напившись вдволь, вы продолжили путь\n"
+            "Перед вами снова три двери\n"
+            "В щель первой двери вы видите яркий свет. За второй дверь слышатся звуки панк-музыки\n"
+            "За третьей вы слышите ветер. Куда пойдем?",
+            reply_markup=markup)
+        return 2
+    elif update.message.text == "2":
+        await stop1(update, context, t=1)
+    elif update.message.text == "3":
+        await stop1(update, context, t=2)
+    else:
+        reply_keyboard = [['1', '2', '3']]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        await update.message.reply_text(
+            "Я вас не понял. Какую дверь вы выбрали? Напишите число.",
+            reply_markup=markup)
+        return 1
+
+
+async def chal_2(update, context):
+    if update.message.text == "1":
+        reply_keyboard = [['1', '2', '3']]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        await update.message.reply_text(
+            "За первой дверью скрывался самый мощныж прожектор Эльбрус 512.172000.1200\n"
+            "Найдя нужную кнопку, вы выключаете его. Перед вами снова три двери\n"
+            "За первой дверью вы слышите звуки волн. За второй дверью - пение птиц\n"
+            "А за третьей - стрельба. Куда пойдем?",
+            reply_markup=markup)
+        return 3
+    elif update.message.text == "2":
+        await stop1(update, context, t=3)
+    elif update.message.text == "3":
+        await stop1(update, context, t=4)
+    else:
+        reply_keyboard = [['1', '2', '3']]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        await update.message.reply_text(
+            "Я вас не понял. Какую дверь вы выбрали? Напишите число.",
+            reply_markup=markup)
+        return 2
+
+
+async def chal_3(update, context):
+    if update.message.text == "3":
+        await update.message.reply_text(
+            "Вы оказались на премьере нового боевика. Он был на столько скучным, что вы заснули\n"
+            "Это был всего лишь сон. Поздравляю, вы справились с квестом\n",
+            reply_markup=ReplyKeyboardRemove())
+        return ConversationHandler.END
+    elif update.message.text == "1":
+        await stop1(update, context, t=5)
+    elif update.message.text == "2":
+        await stop1(update, context, t=6)
+    else:
+        reply_keyboard = [['1', '2', '3']]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        await update.message.reply_text(
+            "Я вас не понял. Какую дверь вы выбрали? Напишите число.",
+            reply_markup=markup)
+        return 3
+
+
+async def stop1(update, context, t=0):
+    if t == 0:
+        await update.message.reply_text(
+            "Вы смирились с безысходностью, присели у  стены лабиринта и уснули на веки вечные. Увы, вы умерли")
+    if t == 1:
+        await update.message.reply_text(
+            "Звук монет оказался звоном капающей непонятной жикости. Дотронувшись до нее вы испарились. Увы, вы умерли")
+    if t == 2:
+        await update.message.reply_text(
+            "Сделав шаг вы провалились в яму и разбились об пики. Увы, вы умерли")
+    if t == 3:
+        await update.message.reply_text(
+            "В нужный момент вы не крикнули 'Панки хой!', вас задавила толпа. Увы, вы умерли")
+    if t == 4:
+        await update.message.reply_text(
+            "Вы попали в внентиляцию, ветер вас понес прямо на вентилятор, где вас раскромсало на кусочки. Увы, "
+            "вы умерли")
+    if t == 5:
+        await update.message.reply_text(
+            "Вы на пляж в самый разгар цунами. Вам не удалось спастись. Увы, вы умерли")
+    if t == 6:
+        await update.message.reply_text(
+            "Вы попали на отвесный уступ скалы. Не найдя другог выхода, вы прыгнули и разбились. Увы, вы умерли")
+    await update.message.reply_text("Всего доброго!", reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
+
+
 async def geocoder(update, context):
     geocoder_uri = "http://geocode-maps.yandex.ru/1.x/"
     response = await get_response(geocoder_uri, params={
@@ -211,8 +323,29 @@ def main():
 
         fallbacks=[CommandHandler('stop', stop)]
     )
+    conv_handler1 = ConversationHandler(
+        # Точка входа в диалог.
+        # В данном случае — команда /start. Она задаёт первый вопрос.
+        entry_points=[CommandHandler('challenge', challenge)],
+
+        # Состояние внутри диалога.
+        # Вариант с двумя обработчиками, фильтрующими текстовые сообщения.
+        states={
+            # Функция читает ответ на первый вопрос и задаёт второй.
+            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, chal_1)],
+            # Функция читает ответ на второй вопрос и завершает диалог.
+            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, chal_2)],
+            3: [MessageHandler(filters.TEXT & ~filters.COMMAND, chal_3)]
+
+        },
+
+        # Точка прерывания диалога. В данном случае — команда /stop.
+        fallbacks=[CommandHandler('stop', stop1)]
+    )
 
     application.add_handler(conv_handler)
+
+    application.add_handler(conv_handler1)
     application.add_handler(CommandHandler('questions', questions))
 
     application.run_polling()
