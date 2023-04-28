dc8665756c85fbb9de1c58181391bdd83a962dda
Akim <akim@saabparts.ru>
2023-04-28 23:44:12 +0300

    +weather bot

--- server.py
+++ server.py
@@ -17,26 +17,6 @@ right_answers = ['9,58 секунд', 'углерод', 'Стэнли Кубри
 all_answers = []
 
 
-def toponym(text):
-    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={text}&format=json"
-
-    # Выполняем запрос.
-    response = requests.get(geocoder_request)
-    if response:
-        # Преобразуем ответ в json-объект
-        json_response = response.json()
-
-        # Получаем первый топоним из ответа геокодера.
-        # Согласно описанию ответа, он находится по следующему пути:
-        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-        # Полный адрес топонима:
-        toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
-        # Координаты центра топонима:
-        toponym_coodrinates = toponym["Point"]["pos"]
-        # Печатаем извлечённые из ответа поля:
-        return toponym_coodrinates
-
-
 async def questions(update, context):
     global all_answers, right_answers
     reply_keyboard = [['9,58 секунд', '9,68 секунд'],
@@ -180,6 +160,119 @@ async def stop(update, context):
     return ConversationHandler.END
 
 
+async def challenge(update, context):
+    reply_keyboard = [['1', '2', '3']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    await update.message.reply_text(
+        "Добро пожаловть в квест!\n"
+        "Вам необходимо пройти небольшое текстое испытание, посредством выбор дверей\n"
+        "Только одна из них ведет на свободу, после других вас ждет смерть\n"
+        "Вы можете прервать квест, послав команду /stop.\n"
+        "Вы просыпаетесь в заброшенной теммнице. Здесь сыровато\n"
+        "На полу вы находите ключ и выбираетесь из темницы\n"
+        "Перед вами три двери. За первой дврью вы слышите детский плач.\n"
+        "За второй дверью слыше звон монет, а за третьей вы слышите лишь пустоту\n"
+        "Куда пойдем?",
+        reply_markup=markup)
+
+    return 1
+
+
+async def chal_1(update, context):
+    if update.message.text == "1":
+        reply_keyboard = [['1', '2', '3']]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        await update.message.reply_text(
+            "Детский плач оказался мелким ручейком. Напившись вдволь, вы продолжили путь\n"
+            "Перед вами снова три двери\n"
+            "В щель первой двери вы видите яркий свет. За второй дверь слышатся звуки панк-музыки\n"
+            "За третьей вы слышите ветер. Куда пойдем?",
+            reply_markup=markup)
+        return 2
+    elif update.message.text == "2":
+        await stop1(update, context, t=1)
+    elif update.message.text == "3":
+        await stop1(update, context, t=2)
+    else:
+        reply_keyboard = [['1', '2', '3']]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        await update.message.reply_text(
+            "Я вас не понял. Какую дверь вы выбрали? Напишите число.",
+            reply_markup=markup)
+        return 1
+
+
+async def chal_2(update, context):
+    if update.message.text == "1":
+        reply_keyboard = [['1', '2', '3']]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        await update.message.reply_text(
+            "За первой дверью скрывался самый мощныж прожектор Эльбрус 512.172000.1200\n"
+            "Найдя нужную кнопку, вы выключаете его. Перед вами снова три двери\n"
+            "За первой дверью вы слышите звуки волн. За второй дверью - пение птиц\n"
+            "А за третьей - стрельба. Куда пойдем?",
+            reply_markup=markup)
+        return 3
+    elif update.message.text == "2":
+        await stop1(update, context, t=3)
+    elif update.message.text == "3":
+        await stop1(update, context, t=4)
+    else:
+        reply_keyboard = [['1', '2', '3']]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        await update.message.reply_text(
+            "Я вас не понял. Какую дверь вы выбрали? Напишите число.",
+            reply_markup=markup)
+        return 2
+
+
+async def chal_3(update, context):
+    if update.message.text == "3":
+        await update.message.reply_text(
+            "Вы оказались на премьере нового боевика. Он был на столько скучным, что вы заснули\n"
+            "Это был всего лишь сон. Поздравляю, вы справились с квестом\n",
+            reply_markup=ReplyKeyboardRemove())
+        return ConversationHandler.END
+    elif update.message.text == "1":
+        await stop1(update, context, t=5)
+    elif update.message.text == "2":
+        await stop1(update, context, t=6)
+    else:
+        reply_keyboard = [['1', '2', '3']]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        await update.message.reply_text(
+            "Я вас не понял. Какую дверь вы выбрали? Напишите число.",
+            reply_markup=markup)
+        return 3
+
+
+async def stop1(update, context, t=0):
+    if t == 0:
+        await update.message.reply_text(
+            "Вы смирились с безысходностью, присели у  стены лабиринта и уснули на веки вечные. Увы, вы умерли")
+    if t == 1:
+        await update.message.reply_text(
+            "Звук монет оказался звоном капающей непонятной жикости. Дотронувшись до нее вы испарились. Увы, вы умерли")
+    if t == 2:
+        await update.message.reply_text(
+            "Сделав шаг вы провалились в яму и разбились об пики. Увы, вы умерли")
+    if t == 3:
+        await update.message.reply_text(
+            "В нужный момент вы не крикнули 'Панки хой!', вас задавила толпа. Увы, вы умерли")
+    if t == 4:
+        await update.message.reply_text(
+            "Вы попали в внентиляцию, ветер вас понес прямо на вентилятор, где вас раскромсало на кусочки. Увы, "
+            "вы умерли")
+    if t == 5:
+        await update.message.reply_text(
+            "Вы на пляж в самый разгар цунами. Вам не удалось спастись. Увы, вы умерли")
+    if t == 6:
+        await update.message.reply_text(
+            "Вы попали на отвесный уступ скалы. Не найдя другог выхода, вы прыгнули и разбились. Увы, вы умерли")
+    await update.message.reply_text("Всего доброго!", reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
+
+
 async def get_response(url, params):
     logger.info(f"getting {url}")
     async with aiohttp.ClientSession() as session:
@@ -187,28 +280,114 @@ async def get_response(url, params):
             return await resp.json()
 
 
-async def yandex_weather(update, context):
-    location = ' '.join(context.args)
-    coords = toponym(location).split()
-    conditions = {'clear': 'ясно', 'partly-cloudy': 'малооблачно', 'cloudy': 'облачно с прояснениями',
-                  'overcast': 'пасмурно', 'drizzle': 'морось', 'light-rain': 'небольшой дождь',
-                  'rain': 'дождь', 'moderate-rain': 'умеренно сильный', 'heavy-rain': 'сильный дождь',
-                  'continuous-heavy-rain': 'длительный сильный дождь', 'showers': 'ливень',
-                  'wet-snow': 'дождь со снегом', 'light-snow': 'небольшой снег', 'snow': 'снег',
-                  'snow-showers': 'снегопад', 'hail': 'град', 'thunderstorm': 'гроза',
-                  'thunderstorm-with-rain': 'дождь с грозой', 'thunderstorm-with-hail': 'гроза с градом'
-                  }
-    wind_dir = {'nw': 'северо-западное', 'n': 'северное', 'ne': 'северо-восточное', 'e': 'восточное',
-                'se': 'юго-восточное', 's': 'южное', 'sw': 'юго-западное', 'w': 'западное', 'с': 'штиль'}
-    params = {"lat": coords[1],
-              "lon": coords[0],
-              "lang": "ru_RU",
-              "hours": "false",
-              "extra": "true"}
-    headers = {"X-Yandex-API-Key": "00e5b9a8-2f6b-4583-9953-3e5c2ff2d16c"}
-    res = requests.get('https://api.weather.yandex.ru/v2/forecast', params=params, headers=headers).json()
+async def weather(update, context):
+    def yandex_weather(data):
+        location = " ".join(data).split()
+        if '--extra' in location:
+            location.remove('--extra')
+        if '-reply' in location:
+            location.remove('-reply')
+        if '-temp' in location:
+            location.remove('-temp')
+        if '-feels_like' in location:
+            location.remove('-feels_like')
+        if '-condition' in location:
+            location.remove('-condition')
+        if '-wind_dir' in location:
+            location.remove('-wind_dir')
+        if '-wind_speed' in location:
+            location.remove('-wind_speed')
+        if 'date:' in location:
+            location.remove('date:')
+            if data.index(data[data.index('date:')]) + 1 != len(data):
+                location.remove(data[data.index('date:') + 1])
+            else:
+                return "Введите дату"
+        location = ' '.join(location)
+        coords = toponym(location).split()
+        conditions = {'clear': 'ясно', 'partly-cloudy': 'малооблачно', 'cloudy': 'облачно с прояснениями',
+                      'overcast': 'пасмурно', 'drizzle': 'морось', 'light-rain': 'небольшой дождь',
+                      'rain': 'дождь', 'moderate-rain': 'умеренно сильный', 'heavy-rain': 'сильный дождь',
+                      'continuous-heavy-rain': 'длительный сильный дождь', 'showers': 'ливень',
+                      'wet-snow': 'дождь со снегом', 'light-snow': 'небольшой снег', 'snow': 'снег',
+                      'snow-showers': 'снегопад', 'hail': 'град', 'thunderstorm': 'гроза',
+                      'thunderstorm-with-rain': 'дождь с грозой', 'thunderstorm-with-hail': 'гроза с градом'
+                      }
+        wind_dir = {'nw': 'северо-западное', 'n': 'северное', 'ne': 'северо-восточное', 'e': 'восточное',
+                    'se': 'юго-восточное', 's': 'южное', 'sw': 'юго-западное', 'w': 'западное', 'с': 'штиль'}
+        params = {"lat": coords[1],
+                  "lon": coords[0],
+                  "lang": "ru_RU",
+                  "extra": "true"}
+        headers = {"X-Yandex-API-Key": "00e5b9a8-2f6b-4583-9953-3e5c2ff2d16c"}
+        res = requests.get('https://api.weather.yandex.ru/v2/forecast', params=params, headers=headers).json()
 
-    await update.message.reply_text(f"Вы выполнили запрос погоды по адресу: {location} \nТемпература: {res['fact']['temp']}°C \nОщущается, как: {res['fact']['feels_like']}°C\nОсадки: {conditions[res['fact']['condition']]}")
+        weather = ''
+        if '-reply' not in data:
+            weather += f"Вы выполнили запрос погоды по адресу: {location}"
+        if '-temp' not in data:
+            weather += f"\nТемпература: {res['fact']['temp']}°C"
+        if '-feels_like' not in data:
+            weather += f"\nОщущается, как: {res['fact']['feels_like']}°C"
+        if '-condition' not in data:
+            weather += f"\nОсадки: {conditions[res['fact']['condition']]}"
+        if '-wind_dir' not in data:
+            weather += f"\nНаправление ветра: {wind_dir[res['fact']['wind_dir']]}"
+        if '-wind_speed' not in data:
+            weather += f"\nСкорость ветра: {res['fact']['wind_speed']} м/с"
+        if "--extra" in data:
+            weather += f"\nСкорость порывов ветра: {res['fact']['wind_gust']} м/с\nДавление : {res['fact']['pressure_mm']}  мм рт. ст.\nВлажность воздуха: {res['fact']['humidity']} %"
+        weather = weather.lstrip("\n")
+        forecasts_date = [res['forecasts'][0]['date'], res['forecasts'][1]['date'], res['forecasts'][2]['date'],
+                          res['forecasts'][3]['date'], res['forecasts'][4]['date'], res['forecasts'][5]['date'],
+                          res['forecasts'][6]['date']]
+        if 'date:' in data:
+            if data[data.index('date:') + 1] in forecasts_date:
+                index = forecasts_date.index(data[data.index('date:') + 1])
+                forecast = ''
+                if '-reply' not in data:
+                    forecast += f"Вы выполнили запрос погоды по адресу: {location}"
+                if '-reply_date' not in data:
+                    forecast += f"\nВы выполнили запрос погоды на дату: {data[data.index('date:') + 1]}"
+                if '-temp' not in data:
+                    forecast += f"\nТемпература: {res['forecasts'][index]['parts']['day_short']['temp']}°C"
+                if '-feels_like' not in data:
+                    forecast += f"\nОщущается, как: {res['forecasts'][index]['parts']['day_short']['feels_like']}°C"
+                if '-condition' not in data:
+                    forecast += f"\nОсадки: {conditions[res['forecasts'][index]['parts']['day_short']['condition']]}"
+                if '-wind_dir' not in data:
+                    forecast += f"\nНаправление ветра: {wind_dir[res['forecasts'][index]['parts']['day_short']['wind_dir']]}"
+                if '-wind_speed' not in data:
+                    forecast += f"\nСкорость ветра: {res['forecasts'][index]['parts']['day_short']['wind_speed']} м/с"
+                if "--extra" in data:
+                    forecast += f"\nСкорость порывов ветра: {res['forecasts'][index]['parts']['day_short']['wind_gust']} м/с\nДавление : {res['forecasts'][index]['parts']['day_short']['pressure_mm']}  мм рт. ст.\nВлажность воздуха: {res['forecasts'][index]['parts']['day_short']['humidity']} %"
+                forecast = forecast.lstrip("\n")
+                return forecast
+            else:
+                return 'введите корректную дату'
+        return weather
+
+    def toponym(text):
+        geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={text}&format=json"
+
+        # Выполняем запрос.
+        response = requests.get(geocoder_request)
+        if response:
+            # Преобразуем ответ в json-объект
+            json_response = response.json()
+
+            # Получаем первый топоним из ответа геокодера.
+            # Согласно описанию ответа, он находится по следующему пути:
+            toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+            # Координаты центра топонима:
+            toponym_coodrinates = toponym["Point"]["pos"]
+            # Печатаем извлечённые из ответа поля:
+            return toponym_coodrinates
+
+    if context.args:
+        await update.message.reply_text(yandex_weather(context.args))
+    else:
+        await update.message.reply_text('Введите геолокацию для вывода погоды')
 
 
 def main():
@@ -255,7 +434,7 @@ def main():
 
     application.add_handler(conv_handler1)
     application.add_handler(CommandHandler('questions', questions))
-    application.add_handler(CommandHandler('weather', yandex_weather))
+    application.add_handler(CommandHandler('weather', weather))
 
     application.run_polling()
 
